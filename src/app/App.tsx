ebc8c140b8b4a45332c101b396eb72d20cd602d9
Anastasiia Bzova <bzovanastia@gmail.com>
2026-05-23 23:29:30 +0200

    Rename component from DetailDrawer to DesktopDetailDrawer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -191,7 +191,7 @@ export default function App() {
       </div>
 
       {/* Detail drawer (desktop) */}
-      <DetailDrawer
+      <DesktopDetailDrawer
         isOpen={isDetailDrawerOpen}
         onClose={handleCloseDetail}
         onChat={() => {
