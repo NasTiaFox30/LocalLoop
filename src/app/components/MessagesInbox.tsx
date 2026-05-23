6e52f8b5200ba1438d465c8faf0f094fa27990ea
Anastasiia Bzova <bzovanastia@gmail.com>
2026-05-23 12:14:19 +0200

    Import conversations from appData (MessagesInbox)

--- src/app/components/MessagesInbox.tsx
+++ src/app/components/MessagesInbox.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { ArrowLeft, Search } from 'lucide-react';
+import { conversations } from '../../data/appData';
 
 interface MessagesInboxProps {
 }
