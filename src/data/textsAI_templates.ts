ca2d73b3a600b4eccc8ad977a52106a7a0d06e5b
Anastasiia Bzova <bzovanastia@gmail.com>
2026-05-24 16:00:22 +0200

    Created textsAI_templates - templates of Favors & Helps texts

---
+++ src/data/textsAI_templates.ts
@@ -0,0 +1,152 @@
+// Szablony tekstów generowanych przez AI
+
+// Szablony dla Ofert (Favor)
+export const offerTemplates = {
+  titles: [
+    'Wiertarka udarowa Bosch Professional',
+    'Sekator ogrodowy Fiskars',
+    'Kosiarka elektryczna Greenworks',
+    'Drabina aluminiowa 4-stopniowa',
+    'Zestaw narzędzi warsztatowych',
+    'Młotek i poziomica laserowa',
+    'Piła stołowa do drewna',
+    'Szlifierka kątowa Bosch',
+    'Wkrętarka akumulatorowa Makita',
+    'Maszyna do szycia Singer',
+    'Odkurzacz przemysłowy Karcher',
+    'Grill gazowy Weber',
+    'Rowerek treningowy',
+    'Toster i opiekacz',
+    'Projektor do prezentacji',
+    'Kamera sportowa GoPro',
+    'Głośnik przenośny JBL',
+    'Nawigacja samochodowa',
+    'Walizka na kołach',
+    'Namiot turystyczny 4-osobowy',
+  ],
+  
+  descriptions: [
+    'Cześć! To moja wiertarka udarowa Bosch Professional, gotowa na Twoje projekty domowe. Jest w świetnym stanie, z baterią i ładowarką. Idealnie sprawdzi się do montażu mebli czy drobnych napraw. Chętnie podzielę się nią z sąsiadami! 🌱',
+    'Mam sekator ogrodowy Fiskars w bardzo dobrym stanie. Idealny do przycinania krzewów, drzewek owocowych i żywopłotu. Ostrza są ostre i dobrze utrzymane. Chętnie użyczę sąsiadom na weekendowe prace w ogrodzie! 🪴',
+    'Kosiarka elektryczna Greenworks - lekka i cicha. Sprawdzi się idealnie na małym i średnim ogrodzie. Posiada regulację wysokości koszenia i pojemny kosz na trawę. Gotowa do użycia od zaraz! 🌿',
+    'Drabina aluminiowa 4-stopniowa, stabilna i lekka. Idealna do prac domowych - malowanie, zmiana żarówek, sięganie do wysokich półek. Składa się kompaktowo, łatwa w przechowywaniu.',
+    'Profesjonalny zestaw narzędzi warsztatowych - klucze nasadowe, kombinowane, imbusy. Świetny dla majsterkowiczów. Wszystkie narzędzia są w idealnym stanie.',
+    'Młotek i poziomica laserowa - niezbędne przy każdym remoncie. Młotek wyważony, poziomica precyzyjna. Wszystko w etui. Chętnie pożyczę sąsiadowi! 🔨',
+    'Piła stołowa do drewna - dla pasjonatów majsterkowania. Sprawdzi się przy cięciu desek i płyt meblowych. Bezpieczna i precyzyjna. Dzielę się z sąsiadami!',
+    'Szlifierka kątowa Bosch - idealna do cięcia i szlifowania metalu. Bardzo mało używana, w oryginalnym opakowaniu. Chętnie użyczę komuś do domowych projektów.',
+    'Wkrętarka akumulatorowa Makita z dwiema bateriami. Bardzo mocna i wydajna. Świetna do mebli i wiercenia. Dzielę się z sąsiadami! 🔧',
+    'Maszyna do szycia Singer - dla osób lubiących szycie i naprawy ubrań. W bardzo dobrym stanie, regularnie serwisowana. Chętnie użyczę!',
+    'Odkurzacz przemysłowy Karcher - idealny do warsztatu, garażu, po remoncie. Mokro-suchy, bardzo mocny. Pożyczę na potrzeby sąsiedzkie!',
+    'Grill gazowy Weber - idealny na rodzinne grille. W bardzo dobrym stanie, regularnie czyszczony. Chętnie podzielę się z sąsiadami! 🍔',
+    'Rowerek treningowy - idealny do domowej siłowni. Mało używany, w idealnym stanie. Pożyczę sąsiadowi na zimowe miesiące! 🚴',
+    'Toster i opiekacz - do szybkiego śniadania. Używany tylko kilka razy. Chętnie oddam w dobre ręce sąsiadowi!',
+    'Projektor do prezentacji - Full HD, z pilotem. Idealny na seanse filmowe w ogrodzie. Pożyczę na weekend! 🎬',
+    'Kamera sportowa GoPro - do nagrywania przygód. W zestawie wszystkie akcesoria. Chętnie podzielę się z sąsiadami!',
+    'Głośnik przenośny JBL - głośnik Bluetooth, bardzo dobra jakość dźwięku. Idealny na imprezy w ogrodzie. Pożyczę! 🎵',
+    'Nawigacja samochodowa - aktualne mapy Europy. Bardzo pomocna w podróży. Chętnie użyczę sąsiadowi na wakacje! 🗺️',
+    'Walizka na kołach - duża pojemność, lekka i wytrzymała. Idealna na podróż. Pożyczę sąsiadowi! 🧳',
+    'Namiot turystyczny 4-osobowy - szybki w rozkładaniu, lekki. Idealny na kemping. Chętnie podzielę się z sąsiadami! ⛺',
+  ],
+  
+  barterOptions: [
+    'Domowe ciasto drożdżowe lub pomoc w ogrodzie',
+    'Świeże warzywa z ogrodu lub domowy dżem',
+    'Pomoc przy myciu okien lub opiece nad kotem',
+    'Kawa lub herbata z domowym ciastkiem',
+    'Pomoc w drobnych naprawach w zamian',
+    'Świeży chleb lub bułki z piekarni',
+    'Godzina pomocy w ogrodzie',
+    'Domowa lemoniada lub kompot',
+    'Książki lub rośliny doniczkowe',
+    'Punkty społecznościowe (30-100)',
+  ],
+};
+
+// Szablony dla Próśb (Help)
+export const helpTemplates = {
+  titles: [
+    'Pomoc w przeprowadzce - wniesienie mebli',
+    'Potrzebuję korepetycji z matematyki',
+    'Naprawa roweru - regulacja przerzutek',
+    'Pomoc w ogrodzie - przycinanie drzew',
+    'Transport mebli z Ikei do domu',
+    'Pomoc w malowaniu pokoju',
+    'Opieka nad kotem w weekend',
+    'Podwiezienie na lotnisko',
+    'Pomoc w nauce angielskiego',
+    'Pożyczenie wiertarki na jeden dzień',
+    'Pomoc w drobnej naprawie hydraulicznej',
+    'Wyprowadzenie psa na spacer',
+    'Pomoc w składaniu mebli z Ikei',
+    'Zbiór jabłek w ogrodzie',
+    'Pomoc w odśnieżaniu podjazdu',
+    'Ktoś z samochodem do przewiezenia paki',
+    'Pomoc w nauce programowania - podstawy',
+    'Pożyczenie drabiny do malowania',
+    'Pomoc w sadzeniu kwiatów w ogrodzie',
+    'Pomoc przy remoncie - zerwanie tapet',
+  ],
+  
+  descriptions: [
+    'Cześć! W przyszły weekend przeprowadzam się na 3 piętro. Niestety nie ma windy, więc potrzebuję pomocy przy wniesieniu cięższych mebli (sofa, lodówka, szafa). Oferuję pizzę i zimne napoje dla pomocników! 🍕',
+    'Szukam korepetytora z matematyki dla mojego syna (14 lat, klasa 8). Potrzebuje pomocy głównie z algebrą i przygotowaniem do egzaminu. Lekcje 2 razy w tygodniu po 60 minut.',
+    'Mój rower wymaga regulacji przerzutek i hamulców. Jestem samoukiem, ale boję się coś zepsuć. Ktoś doświadczony mógłby pomóc? W zamian mogę pomóc w ogrodzie lub upiec ciasto! 🚲',
+    'Mam spory ogród, który wymaga przycięcia drzew owocowych i krzewów. Nie mam odpowiednich narzędzi ani doświadczenia. Ktoś pomoże w sobotę?',
+    'Kupiłam nową sofę w Ikei, ale nie mam jak jej przewieźć. Czy ktoś z samochodem dostawczym lub dużym kombi mógłby pomóc? Trasa: Ikea -> moje mieszkanie (ok. 5km).',
+    'Chcę odświeżyć pokój córki - pomalować ściany na jasny kolor. Potrzebuję kogoś do pomocy, sama nie dam rady. Materiały zapewniam, szukam rąk do pracy!',
+    'Wyjazd na weekend, potrzebuję kogoś do opieki nad kotem. Łatwy w opiece, tylko jedzenie i głaskanie. Chętnie oddam przysługę w przyszłości! 🐱',
+    'Potrzebuję podwiezienia na lotnisko Okęcie w piątek rano (ok. godz. 6). Dorzucę się do paliwa i kupię kawę ☕',
+    'Szukam kogoś do pomocy w nauce angielskiego - poziom średniozaawansowany. Chcę poćwiczyć konwersacje. W zamian mogę pomóc z matematyką lub informatyka!',
+    'Czy ktoś miałby wiertarkę na jeden dzień? Muszę zawiesić półki w salonie. W zamian upiekę ciasto lub pomogę w ogrodzie 🍰',
+    'Ktoś ogarnięty w hydraulice? Mam małą nieszczelność pod zlewem, boję się ingerować samemu. W zamian obiad lub pomoc w czymś innym!',
+    'Złamałem nogę i potrzebuję kogoś do wyprowadzenia psa na krótki spacer (2 razy dziennie) przez najbliższy tydzień. Bardzo proszę o pomoc! 🐕',
+    'Kupiłem nową szafę z Ikei i potrzebuję pomocy przy składaniu. Instrukcja jest po polsku, ale na pewno potrzebne będą dodatkowe ręce. Oferuję obiad i piwo! 🍺',
+    'Mam ogród pełen jabłoni. Owoców jest bardzo dużo. Potrzebuję pomocy przy zbiorze - zabierz tyle jabłek ile chcesz! Mogę też upiec jabłecznik w zamian 🍎',
+    'Zima tuż tuż, a mam długi podjazd. Czy ktoś z pługiem lub odśnieżarką mógłby pomóc gdy spadnie śnieg? Oferuję domowe ciasto w zamian ❄️',
+    'Potrzebuję przewieźć deskę i elementy do majsterkowania. Samochód byłby bardzo pomocny. Oferuję 50 punktów społecznościowych i kawę po drodze!',
+    'Uczę się programowania i potrzebuję mentora do pomocy z Pythonem. Podstawy ogarniam, ale potrzebuję wsparcia. W zamian mogę pomóc w marketingu lub social media! 💻',
+    'Muszę pomalować sufit, ale moja drabina jest za niska. Czy ktoś ma wyższą drabinę na pożyczenie na jeden dzień? Będę wdzięczny!',
+    'W ogrodzie sadzę nowe kwiaty - potrzebuję dodatkowej pary rąk na sobotę. W zamian poczęstuję domowym jedzeniem i napojami! 🌷',
+    'Remontuję pokój i potrzebuję pomocy przy zerwaniu starych tapet. Praca na kilka godzin, w zamian pizza i piwo! 🍕',
+  ],
+  
+  barterOptions: [
+    'Domowe ciasto lub pizza',
+    'Pomoc w ogrodzie w zamian',
+    'Kawa i domowe wypieki',
+    'Punkty społecznościowe (50-100)',
+    'Wymiana na książki lub rośliny doniczkowe',
+    'Obiad domowego gotowania',
+    'Ciasto drożdżowe lub szarlotka',
+    'Pomoc w drobnych naprawach',
+    'Oddanie przysługi w przyszłości',
+    'Domowa nalewka lub sok',
+  ],
+};
+
+// Funkcje pomocnicze do generowania losowych treści
+export const generateRandomOfferContent = () => {
+  return {
+    title: offerTemplates.titles[Math.floor(Math.random() * offerTemplates.titles.length)],
+    description: offerTemplates.descriptions[Math.floor(Math.random() * offerTemplates.descriptions.length)],
+    suggestedBarter: offerTemplates.barterOptions[Math.floor(Math.random() * offerTemplates.barterOptions.length)],
+    suggestedPoints: Math.floor(Math.random() * 100) + 20,
+  };
+};
+
+export const generateRandomHelpContent = () => {
+  return {
+    title: helpTemplates.titles[Math.floor(Math.random() * helpTemplates.titles.length)],
+    description: helpTemplates.descriptions[Math.floor(Math.random() * helpTemplates.descriptions.length)],
+    suggestedBarter: helpTemplates.barterOptions[Math.floor(Math.random() * helpTemplates.barterOptions.length)],
+    suggestedPoints: Math.floor(Math.random() * 100) + 30,
+  };
+};
+
+// Eksport domyślny dla wygody
+export const aiTemplates = {
+  offer: offerTemplates,
+  help: helpTemplates,
+  generateOffer: generateRandomOfferContent,
+  generateHelp: generateRandomHelpContent,
+};
